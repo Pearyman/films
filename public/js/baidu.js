afac173ae25330e0ed76cac87bbbd359ce147674
yanan lee <liyananhappy@sina.cn>
2017-10-24 00:39:08 +0800

    beta 0.0.2

---
+++ public/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8168a7e318f40183c8e6dd50b02d0c88";;
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
